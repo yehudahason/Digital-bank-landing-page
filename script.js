0e0d0f08081f9dd8bade816ea495c28190ab5fe9
Yehuda Hason <yehudaxt@gmail.com>
2026-04-29 22:11:31 +0300

    script js add

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const menuBtn = document.querySelector(".hamburger");
 const menuEl = document.querySelector(".ul-container");
+const body = document.querySelector("body");
 
 menuBtn.addEventListener("click", () => {
   menuEl.classList.toggle("active");
   menuBtn.classList.toggle("active");
+  body.classList.toggle("active");
 });
 
 window.addEventListener("click", (e) => {
   if (e.target !== menuBtn && !menuEl.contains(e.target)) {
     menuEl.classList.remove("active");
     menuBtn.classList.remove("active");
+    body.classList.remove("active");
   }
 });
